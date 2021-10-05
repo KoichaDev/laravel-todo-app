36835701dbee5815ce2181d8bde234ecdf5974bd
WSL Koicha <khoii87@gmail.com>
2021-10-05 02:29:24 +0200

    Add: 📦 postcss-custom-properties

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,12 +12,19 @@ const mix = require('laravel-mix');
  */
 
  mix.sass("resources/scss/main.scss", "public/css").options({
+    postCss: [
+        require('postcss-custom-properties')({
+            preserve: false
+        })
+    ],
     autoprefixer: {
         options: {
-            browsers: ["last 6 versions"],
-        },
+            browsers: [
+                'last 6 versions',
+            ]
+        }
     },
-})
+});
 
 mix.browserSync({
     proxy: "http://127.0.0.1:8000",
