c7ba2bc1529259667fb335e69c77d55e82d1a1f9
WSL Koicha <khoii87@gmail.com>
2021-10-05 02:57:37 +0200

    Add: ✨ Laravel Mix Options for Production

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ const mix = require('laravel-mix');
 // Enable the long-term caching which enables file hashing that Laravel Mix provides.
 mix.inProduction() ? serveProductionMode() : serveDevelopmentMode();
 
+
 function serveDevelopmentMode() {
     mix.
         sass("resources/scss/main.scss", "public/css").options({
@@ -26,23 +27,26 @@ function serveDevelopmentMode() {
 }
 
 function serveProductionMode() {
-    mix.
-        sass("resources/scss/main.scss", "public/css").options({
-            postCss: [
-                require('postcss-custom-properties')({
-                    preserve: false
-                })
-            ],
-            autoprefixer: {
-                options: {
-                    browsers: [
-                        'last 6 versions',
-                    ]
-                }
-            },
-        }).ts('resources/ts/app.ts', 'public/js').version();
-
-    mix.browserSync({
-        proxy: "http://127.0.0.1:8000",
-    });
+    mix.sass("resources/scss/main.scss", "public/css").options({
+        processCssUrls: false,
+        terser: {},
+        purifyCss: false,
+        postCss: [require('autoprefixer')],
+        clearConsole: false,
+        cssNano: {
+            discardComments: { removeAll: true },
+        },
+        postCss: [
+            require('postcss-custom-properties')({
+                preserve: false
+            })
+        ],
+        autoprefixer: {
+            options: {
+                browsers: [
+                    'last 6 versions',
+                ]
+            }
+        },
+    }).ts('resources/ts/app.ts', 'public/js').version();
 }
