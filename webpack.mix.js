62dfee02844a25d6062378395c9dbbbcc7b1f504
WSL Koicha <khoii87@gmail.com>
2021-10-05 02:45:56 +0200

    Add: ✨  Laravel Mix provides version() for caching the hashing
    https://laravel-mix.com/docs/5.0/versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,22 +1,48 @@
 const mix = require('laravel-mix');
 
-mix.
- sass("resources/scss/main.scss", "public/css").options({
-    postCss: [
-        require('postcss-custom-properties')({
-            preserve: false
-        })
-    ],
-    autoprefixer: {
-        options: {
-            browsers: [
-                'last 6 versions',
-            ]
-        }
-    },
-}).ts('resources/ts/app.ts', 'public/js').version();
+// Enable the long-term caching which enables file hashing that Laravel Mix provides.
+mix.inProduction() ? serveProductionMode() : serveDevelopmentMode();
 
-mix.browserSync({
-    proxy: "http://127.0.0.1:8000",
-});
+function serveDevelopmentMode() {
+    mix.
+        sass("resources/scss/main.scss", "public/css").options({
+            postCss: [
+                require('postcss-custom-properties')({
+                    preserve: false
+                })
+            ],
+            autoprefixer: {
+                options: {
+                    browsers: [
+                        'last 6 versions',
+                    ]
+                }
+            },
+        }).ts('resources/ts/app.ts', 'public/js');
 
+    mix.browserSync({
+        proxy: "http://127.0.0.1:8000",
+    });
+}
+
+function serveProductionMode() {
+    mix.
+        sass("resources/scss/main.scss", "public/css").options({
+            postCss: [
+                require('postcss-custom-properties')({
+                    preserve: false
+                })
+            ],
+            autoprefixer: {
+                options: {
+                    browsers: [
+                        'last 6 versions',
+                    ]
+                }
+            },
+        }).ts('resources/ts/app.ts', 'public/js').version();
+
+    mix.browserSync({
+        proxy: "http://127.0.0.1:8000",
+    });
+}
