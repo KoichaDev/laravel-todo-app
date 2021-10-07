89699024518537b5d6bf97b89a01bba3094122bd
WSL Koicha <khoii87@gmail.com>
2021-10-07 22:51:02 +0200

    Update: 💄 Button Colors and modified element of the button style

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar">
     <img src="/images/todo-icon.png" alt="todo-icon" width="25" height="25" class="navbar__image">
-    <a href="#" class="navbar__login">Login</a>
-    <a href="" class="navbar__sign-up btn">Sign up</a>
+    <a href="#" class="navbar__login btn btn--link">Login</a>
+    <a href="" class="navbar__sign-up btn btn--info">Sign up</a>
 </nav>
