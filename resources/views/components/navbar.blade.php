bda7b314f6b87f430365f3c296d9b88f89c838e9
WSL Koicha <khoii87@gmail.com>
2021-10-09 15:01:23 +0200

    Merge: 🔀 branch 'feature/page/home' into main
    Init: 🎉 Simple home page screen

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar">
-    <img src="/images/todo-icon.png" alt="todo-icon" width="25" height="25" class="navbar__image">
+    <img src="/images/todo-icon.png" alt="todo-icon" width="25" height="25" class="navbar__logo">
     <a href="#" class="navbar__text">lara.todo</a>
     <a href=" #" class="navbar__login btn btn--link">Login</a>
     <a href="" class="navbar__sign-up btn btn--primary">Sign up</a>
---
+++ resources/views/components/pages/home.blade.php
@@ -0,0 +1,16 @@
+<main class="home-page">
+    <section class="home-page__landing-page1">
+        <div class="home-page__landing-page-sub-container">
+            <div class="home-page__landing-page2-article">
+                <h1>Organize your daily activity with <span>lara.todo</span></h1>
+                <p>All your work in one place: Tasks, docs, chat, goals, & more.</p>
+                <a href=" #" tabindex="0" role="button" aria-label="Click here to learn more"
+                   class="btn btn--success">Click
+                    Here to learn more</a>
+            </div>
+            <img class="home-page__landing-page2-image" src="/images/sprint-graphic.png"
+                 alt="lara.todo illustrator image">
+        </div>
+    </section>
+
+</main>
